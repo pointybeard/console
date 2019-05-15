599412aa055673b6fa3be2c8f0b3a808137c13e7
Alannah Kearney <hi@alannahkearney.com>
2019-05-15 11:57:30 +0000

    Made improvements to console_fatal_error()

--- src/Includes/Functions.php
+++ src/Includes/Functions.php
@@ -1,5 +1,8 @@
 <?php declare(strict_types=1);
 
+use pointybeard\Helpers\Functions\Strings;
+use pointybeard\Helpers\Functions\Arrays;
+
 /**
  * Convienence function for displaying a red console error without using
  * the Console/Message class
@@ -7,20 +10,63 @@
 if (!function_exists("console_fatal_error")) {
     function console_fatal_error($message, $heading = "SYMPHONY CONSOLE FATAL ERROR") : void
     {
-
-        $wrap_with_red = function(string $string, int $pad = 5, bool $bold = false) : string {
+        $wrap_with_red = function (string $string, int $pad = 5, bool $bold = false) : string {
             $string = str_pad('', $pad, ' ') . $string . str_pad('', $pad, ' ');
             return "\e[" . ($bold ? 1 : 0) . ";37m\e[41m{$string}\033[0m";
         };
 
-        $heading = str_pad($heading, 40, ' ', \STR_PAD_RIGHT);
+        $emptyLine = '    ' . $wrap_with_red(str_pad('', 40, ' ', \STR_PAD_RIGHT), 5, true);
+        $heading = str_pad(trim($heading), 40, ' ', \STR_PAD_RIGHT);
 
-        $message = utf8_wordwrap_array($message, strlen($heading) - 10);
-        for($ii = 0; $ii < count($message); $ii++) {
-            $message[$ii] = "    " . $wrap_with_red(str_pad($message[$ii], strlen($heading), ' ', \STR_PAD_RIGHT));
+        $message = Strings\utf8_wordwrap_array($message, strlen($heading) - 10);
+
+        // Remove surrounding whitespace
+        $message = array_map("trim", $message);
+
+        // Remove empty elements from the array
+        $message = Arrays\array_remove_empty($message);
+
+        // Reset array indicies
+        $message = array_values($message);
+
+        // Check for a backtrace and get it's index if there is one
+        $traceArrayIndex = array_search("Trace", $message);
+        if ($traceArrayIndex !== false) {
+            // Purely cosmetic; add a new line before the trace starts
+            $message[$traceArrayIndex] = PHP_EOL . $message[$traceArrayIndex];
         }
 
-        printf(PHP_EOL . '    %s%3$s%s%3$s' .  PHP_EOL, $wrap_with_red($heading, 5, true), implode($message, PHP_EOL), PHP_EOL);
+        // Wrap everything, except a trace, in red
+        for ($ii = 0; $ii < count($message); $ii++) {
+            if ($traceArrayIndex !== false && $ii == $traceArrayIndex) {
+                break;
+            }
+            $message[$ii] = '    ' . $wrap_with_red(str_pad(
+                $message[$ii],
+                strlen($heading),
+                ' ',
+                \STR_PAD_RIGHT
+            ));
+        }
+
+        // Add an empty red line before the trace (or at the end if there
+        // is no trace)
+        Arrays\array_insert_at_index(
+            $message,
+            $traceArrayIndex !== false
+                ? $traceArrayIndex
+                : count($message),
+            $emptyLine
+        );
+
+        // Print the error message, starting with an empty red line
+        printf(
+            "\r\n%s\r\n    %s\r\n%s\r\n",
+            $emptyLine,
+            $wrap_with_red($heading, 5, true),
+            implode($message, PHP_EOL)
+        );
+
         exit(1);
     }
 }
