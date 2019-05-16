b9ff14d436e3a70b5d2eb25f4a1d0511b5eb61aa
Alannah Kearney <hi@alannahkearney.com>
2019-05-16 10:29:43 +0000

    Moved commands from /bin to /commands

--- commands/hello.php
+++ commands/hello.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Symphony\Console\Command\Console;
+namespace Symphony\Console\Commands\Console;
 
 use Symphony\Console as Console;
 use Symphony\Console\AbstractInputType as Type;
