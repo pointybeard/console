f980a374a99aaf9bd16b762090512fe728c3eb9c
Alannah Kearney <hi@alannahkearney.com>
2019-06-02 09:25:02 +0000

    Updated example command 'hello' based on changes in pointybeard/helpers-cli-input v1.2

--- commands/hello.php
+++ commands/hello.php
@@ -16,12 +16,13 @@ class Hello extends Console\AbstractCommand
     {
         parent::__construct();
         $this
-            ->description('echoes all arguments')
+            ->description('echoes all arguments, options, and flags')
             ->version('1.0.1')
             ->example(
                 'symphony console hello -a 123 --bravo=456'
             )
-            ->support("If you believe you have found a bug, please report it using the GitHub issue tracker at https://github.com/pointybeard/console/issues, or better yet, fork the library and submit a pull request.\r\n\r\nCopyright 2015-2019 Alannah Kearney. Use 'symphony -L' to see software licence information.\r\n")
+            ->support("If you believe you have found a bug, please report it using the GitHub issue tracker at https://github.com/pointybeard/console/issues, or better yet, fork the library and submit a pull request.\r\n\r\nCopyright 2015-2019 Alannah Kearney. See ".realpath(__DIR__.'/../LICENCE')." for software licence information.\r\n")
+            ->bindFlags(Input\AbstractInputHandler::FLAG_VALIDATION_SKIP_UNRECOGNISED)
         ;
     }
 
@@ -41,47 +42,57 @@ class Hello extends Console\AbstractCommand
             ->foreground(Colour::FG_GREEN)
             ->display()
         ;
-        foreach ($input->getArguments() as $name => $value) {
+
+        $position = 0;
+        foreach ($input->getCollection()->getItemsByType('Argument') as $item) {
             (new Message())
                 ->message(sprintf(
                     ' %d: %s => %s',
-                    $count,
-                    $name,
-                    $value
+                    $position + 1,
+                    $item->name(),
+                    $input->find($item->name)
                 ))
                 ->display()
             ;
-            ++$count;
+            ++$position;
         }
         echo PHP_EOL;
 
-        (new Message('OPTIONS'))
+        (new Message('OPTIONS & FLAGS'))
             ->foreground(Colour::FG_GREEN)
             ->display()
         ;
 
-        foreach ($input->getOptions() as $name => $value) {
-            $o = $input->getCollection()->findOption($name);
-
-            $name = $o instanceof Input\Types\Option
-                ? $o->name()
-                : $name;
-
-            $long = $o instanceof Input\Types\Option && null !== $o->long()
-                ? $o->long()
-                : null;
-
-            $args = [
-                strlen($name) > 1 ? '-' : '',
-                $name,
-                null !== $long ? " (--{$long}) " : ' ',
-                Strings\type_sensitive_strval($value),
-            ];
-
+        foreach ($input->getCollection()->getItemsExcludeByType('Argument') as $item) {
+            if (null === ($value = $input->find($item->name()))) {
+                continue;
+            }
             (new Message())
-                ->message(vsprintf(
-                    ' -%s%s%s=> %s',
-                    $args
+                ->message(sprintf(
+                    ' %s => %s',
+                    $item->getDisplayName(),
+                    Strings\type_sensitive_strval($value)
+                ))
+                ->display()
+            ;
+        }
+
+        echo PHP_EOL;
+
+        (new Message('UNRECOGNISED'))
+            ->foreground(Colour::FG_GREEN)
+            ->display()
+        ;
+
+        foreach ($input->getInput() as $name => $value) {
+            if (null !== $input->getCollection()->find((string) $name)) {
+                continue;
+            }
+            (new Message())
+                ->message(sprintf(
+                    '%s => %s',
+                    is_int($name) ? '[ARGUMENT]' : $name,
+                    Strings\type_sensitive_strval($value)
                 ))
                 ->display()
             ;
