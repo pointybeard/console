2a7e4b2bd9dc03502539b145feb464c252bc95d7
Alannah Kearney <hi@alannahkearney.com>
2019-06-02 09:27:53 +0000

    Removed addArgument(), addOption(), and addFlag() methods from InputHandlerInterface. init() now returns void instead of bool

--- src/Console/Interfaces/CommandInterface.php
+++ src/Console/Interfaces/CommandInterface.php
@@ -9,12 +9,5 @@ use pointybeard\Helpers\Cli\Input;
 interface CommandInterface
 {
     public function execute(Input\Interfaces\InputHandlerInterface $input): bool;
-
-    public function init(): bool;
-
-    public function addArgument(string $name, int $flags = null, string $description = null, object $validator = null, bool $replaceExisting = false): object;
-
-    public function addOption(string $name, string $long = null, int $flags = null, string $description = null, object $validator = null, $default = null, bool $replaceExisting = false): object;
-
-    public function addFlag(string $name, int $flags = null, string $description = null, $default = false): object;
+    public function init(): void;
 }
