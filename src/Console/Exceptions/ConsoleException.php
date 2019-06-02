36c01e705f02c250380fc34eca7e827d695494cc
Alannah Kearney <hi@alannahkearney.com>
2019-06-02 09:28:43 +0000

    ConsoleException will show trace of previous exception if it's an instance of ReadableTraceException

--- src/Console/Exceptions/ConsoleException.php
+++ src/Console/Exceptions/ConsoleException.php
@@ -8,4 +8,16 @@ use pointybeard\Helpers\Exceptions\ReadableTrace;
 
 class ConsoleException extends ReadableTrace\ReadableTraceException
 {
+    public function getReadableTrace(string $format = '[{{PATH}}/{{FILENAME}}:{{LINE}}] {{CLASS}}{{TYPE}}{{FUNCTION}}();'): ?string
+    {
+        // The trace of any previous exception that is an instance of
+        // ReadableTraceException is likely to be much more informative
+        // so use that intead
+        if ($this->getPrevious() instanceof \pointybeard\Helpers\Exceptions\ReadableTrace\ReadableTraceException) {
+            return $this->getPrevious()->getReadableTrace();
+        }
+
+        // Otherwise, default to whatever trace we have
+        return parent::getReadableTrace();
+    }
 }
