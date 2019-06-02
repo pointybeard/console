4e279a20def129b0bac98d7555557b2539d4666c
Alannah Kearney <hi@alannahkearney.com>
2019-06-02 09:34:28 +0000

    Simplified ExceptionHandler. No longer extends GenericExceptionHandler. Removed custom trace generation. Using display_error_and_exit() from pointybeard/helpers-functions-cli to render the error.

--- src/Console/ExceptionHandler.php
+++ src/Console/ExceptionHandler.php
@@ -4,88 +4,55 @@ declare(strict_types=1);
 
 namespace Symphony\Console;
 
-use Exception;
-use GenericExceptionHandler;
+use pointybeard\Helpers\Functions\Cli;
+use pointybeard\Helpers\Cli\Colour\Colour;
 
-class ExceptionHandler extends GenericExceptionHandler
+class ExceptionHandler
 {
-    public static function initialise(?\Log $Log = null)
+    public static $enabled = true;
+    private static $log = null;
+
+    public static function initialise(?\Log $log = null)
     {
         self::$enabled = true;
 
-        if (null !== $Log) {
-            self::$_Log = $Log;
+        if (null !== $log) {
+            self::$log = $log;
         }
 
-        // Symphony's exception handler is set twice. Call restore_error_handler
-        // twice to clear it out.
+        // Symphony's exception handler is set twice so we need to call
+        // restore_error_handler() twice to clear it out.
         restore_exception_handler();
         restore_exception_handler();
 
         set_exception_handler(array(__CLASS__, 'handler'));
     }
 
-    public static function handler($e)
+    public static function handler($ex): void
     {
         try {
             if (true !== self::$enabled) {
                 return;
             }
 
-            $class = __CLASS__;
-            $exception_type = get_class($e);
-            if (class_exists("{$exception_type}Handler") && method_exists("{$exception_type}Handler", 'render')) {
-                $class = "{$exception_type}Handler";
-            }
-
-            echo call_user_func(array($class, 'render'), $e);
-        } catch (Exception $e) {
-            echo 'Looks like the Exception handler crapped out';
-            print_r($e);
-        }
-
-        exit();
-    }
-
-    public static function render($e)
-    {
-        $lines = null;
-
-        foreach (self::__nearByLines($e->getLine(), $e->getFile()) as $line => $string) {
-            $lines .= sprintf(
-                '%d: %s',
-                ++$line,
-                $string
+            Cli\display_error_and_exit(
+                sprintf(
+                    "An uncaught exception occurred in %s around line %d:\r\n%s",
+                    $ex->getFile(),
+                    $ex->getLine(),
+                    $ex->getMessage(),
+                    ),
+                sprintf('[%s]', (new \ReflectionClass($ex))->getName()),
+                Colour::BG_RED,
+                $ex->getPrevious() instanceof \Exception
+                    ? $ex->getPrevious()->getTrace()
+                    : $ex->getTrace()
             );
+        } catch (\Exception $ex) {
+            echo 'Looks like the Exception handler crapped out';
+            print_r($ex);
         }
 
-        $trace = null;
-
-        if (count($e->getTrace()) > 0) {
-            foreach ($e->getTrace() as $t) {
-                $trace .= sprintf(
-                    '[%s:%d] %s%s%s();'."\n",
-                    (isset($t['file']) ? $t['file'] : null),
-                    (isset($t['line']) ? $t['line'] : null),
-                    (isset($t['class']) ? $t['class'] : null),
-                    (isset($t['type']) ? $t['type'] : null),
-                    $t['function']
-                );
-            }
-        }
-
-        return sprintf(
-            '%s: %s
-
-An error occurred in %s around line %d
-%s
-%s',
-            ($e instanceof ErrorException ? GenericErrorHandler::$errorTypeStrings[$e->getSeverity()] : 'Fatal Error'),
-            $e->getMessage(),
-            $e->getFile(),
-            $e->getLine(),
-            $lines,
-            (null !== $trace ? 'Backtrace'.PHP_EOL.'==========================='.PHP_EOL.$trace.PHP_EOL : null)
-        );
+        exit(1);
     }
 }
