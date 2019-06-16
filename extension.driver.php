097f14508a4b7455ef8863a45c09ed1207a84e3f
Alannah Kearney <hi@alannahkearney.com>
2019-06-16 00:12:18 +0000

    Added some additional logic to Extension driver to check if the vendor/autoload.php exists and to ensure the Extension_Console driver class is not created more than once.

--- extension.driver.php
+++ extension.driver.php
@@ -2,6 +2,24 @@
 
 declare(strict_types=1);
 
-class extension_Console extends Extension
-{
+if (!file_exists(__DIR__.'/vendor/autoload.php')) {
+    throw new Exception(sprintf(
+        'Could not find composer autoload file %s. Did you run `composer update` in %s?',
+        __DIR__.'/vendor/autoload.php',
+        __DIR__
+    ));
+}
+
+require_once __DIR__.'/vendor/autoload.php';
+
+// This file is included automatically in the composer autoloader, however,
+// Symphony might try to include it again which would cause a fatal error.
+// Check if the class already exists before declaring it again.
+if (!class_exists('\\Extension_Console')) {
+    class Extension_Console extends Extension
+    {
+        public static function init()
+        {
+        }
+    }
 }
