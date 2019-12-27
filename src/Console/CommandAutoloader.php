dda5b1cf3ecd5e79200b66f383c5b77d19506cc6
Alannah Kearney <hi@alannahkearney.com>
2019-12-27 05:52:20 +0000

    Further improvements to command autoloader to reduce issues with case sensitivity

--- src/Console/CommandAutoloader.php
+++ src/Console/CommandAutoloader.php
@@ -43,45 +43,55 @@ final class CommandAutoloader
 
             $filepaths = [];
 
-            if (0 == strcasecmp($extension, 'workspace')) {
-                $filepaths[] = sprintf(
-                    '%s/commands/%s.php',
-                    WORKSPACE,
-                    ucfirst($command)
-                );
+            $generateFilePathCombinations = function($subjects, string $delim=DIRECTORY_SEPARATOR, string $pre=null, string $post=null, array $operations = ["strtolower", "ucfirst", "lcfirst"]): array {
+                if(!is_array($subjects)) {
+                    $subjects = [$subjects];
+                }
 
-                $filepaths[] = sprintf(
-                    '%s/commands/%s.php',
-                    WORKSPACE,
-                    strtolower($command)
+                $sets = array_fill_keys($subjects, []);
+
+                foreach($subjects as $s) {
+                    $sets[$s][] = $s;
+                    foreach($operations as $func) {
+                        $sets[$s][] = call_user_func($func, $s);
+                    }
+                    $sets[$s] = array_unique($sets[$s]);
+                }
+
+                $combinations = [[]];
+                foreach($sets as $index => $values) {
+                    $tmp = [];
+                    foreach($combinations as $r) {
+                        foreach($values as $v) {
+                            $r[$index] = $v;
+                            $tmp[] = $r;
+                        }
+                    }
+                    $combinations = $tmp;
+                }
+
+                $result = [];
+                foreach($combinations as $items) {
+                    $result[] = $pre . implode($items, $delim) . $post;
+                }
+
+                return array_unique($result);
+
+            };
+
+            if (0 == strcasecmp($extension, 'workspace')) {
+                $filepaths = $generateFilePathCombinations(
+                    ['commands', $command],
+                    DIRECTORY_SEPARATOR,
+                    WORKSPACE . DIRECTORY_SEPARATOR,
+                    ".php"
                 );
             } else {
-                $filepaths[] = sprintf(
-                    '%s/%s/commands/%s.php',
-                    EXTENSIONS,
-                    ucfirst($extension),
-                    ucfirst($command)
-                );
-
-                $filepaths[] = sprintf(
-                    '%s/%s/commands/%s.php',
-                    EXTENSIONS,
-                    strtolower($extension),
-                    strtolower($command)
-                );
-
-                $filepaths[] = sprintf(
-                    '%s/%s/commands/%s.php',
-                    EXTENSIONS,
-                    ucfirst($extension),
-                    strtolower($command)
-                );
-
-                $filepaths[] = sprintf(
-                    '%s/%s/commands/%s.php',
-                    EXTENSIONS,
-                    strtolower($extension),
-                    ucfirst($command)
+                $filepaths = $generateFilePathCombinations(
+                    [$extension, 'commands', $command],
+                    DIRECTORY_SEPARATOR,
+                    EXTENSIONS . DIRECTORY_SEPARATOR,
+                    ".php"
                 );
             }
 
